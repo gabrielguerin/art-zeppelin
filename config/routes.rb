270d05908157b0366635d042eaaa16f92e3b5c72
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-03-21 14:39:40 +0100

    admin dashboard now working

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :admin do
-      resources :users
-      resources :artists
-      resources :artists_tags
-      resources :artworks
-      resources :artworks_tags
-      resources :blogs
-      resources :blogs_tags
-      resources :clients
-      resources :companies
-      resources :projects
-      resources :projects_tags
-      resources :tags
+    resources :users
+    resources :artists
+    resources :artists_tags
+    resources :artworks
+    resources :artworks_tags
+    resources :blogs
+    resources :blogs_tags
+    resources :clients
+    resources :companies
+    resources :projects
+    resources :projects_tags
+    resources :tags
 
-      root to: "users#index"
-    end
+    root to: 'users#index'
+  end
   resources :blogs
   devise_for :users
   resources :projects
---
+++ db/schema.rb
@@ -0,0 +1,166 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_20_170149) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.string "email"
+    t.string "telephone"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_artists_on_slug", unique: true
+  end
+
+  create_table "artists_tags", force: :cascade do |t|
+    t.bigint "artist_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_artists_tags_on_artist_id"
+    t.index ["tag_id"], name: "index_artists_tags_on_tag_id"
+  end
+
+  create_table "artworks", force: :cascade do |t|
+    t.bigint "artist_id"
+    t.string "title"
+    t.text "description"
+    t.string "dimension"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["artist_id"], name: "index_artworks_on_artist_id"
+    t.index ["slug"], name: "index_artworks_on_slug", unique: true
+  end
+
+  create_table "artworks_tags", force: :cascade do |t|
+    t.bigint "artwork_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artwork_id"], name: "index_artworks_tags_on_artwork_id"
+    t.index ["tag_id"], name: "index_artworks_tags_on_tag_id"
+  end
+
+  create_table "blogs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "blogs_tags", force: :cascade do |t|
+    t.bigint "blog_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_id"], name: "index_blogs_tags_on_blog_id"
+    t.index ["tag_id"], name: "index_blogs_tags_on_tag_id"
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.bigint "company_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "job"
+    t.string "gender"
+    t.string "address"
+    t.string "telephone"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["company_id"], name: "index_clients_on_company_id"
+    t.index ["slug"], name: "index_clients_on_slug", unique: true
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "size"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_companies_on_slug", unique: true
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "artist_id"
+    t.bigint "client_id"
+    t.datetime "date"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_projects_on_artist_id"
+    t.index ["client_id"], name: "index_projects_on_client_id"
+  end
+
+  create_table "projects_tags", force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_projects_tags_on_project_id"
+    t.index ["tag_id"], name: "index_projects_tags_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_tags_on_slug", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.boolean "admin"
+    t.string "slug"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
+  end
+
+end
