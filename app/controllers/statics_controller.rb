a0fd5cf3e2d02110e5022eabf4bec6c92b4bb9da
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-04-05 13:53:19 +0200

    contact forms now working

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class StaticsController < ApplicationController
-  
   def show; end
+
+  def for_artists; end
+
+  def for_companies; end
+
+  def contact
+    @client = Client.new
+  end
+
+  def deductions; end
 end
