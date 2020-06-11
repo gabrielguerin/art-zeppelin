ca1fe11b6a56d98ba4b1e7ee9fda1dde6956c67e
Gabriel Guérin <gabriel.gueringg@gmail.com>
2020-06-11 12:40:56 +0200

    Added search functionality to artists

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
-  searchkick word_start: %i[title]
-  extend FriendlyId  
+  searchkick
+
+  extend FriendlyId
+
   friendly_id :title, use: :slugged
+
   has_rich_text :body
+
   has_one_attached :cover
 
   belongs_to :user
+
   has_many :blogs_tags, dependent: :destroy
+
   has_many :tags, through: :blogs_tags
 end
